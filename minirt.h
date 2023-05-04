76f7a593253f8eb32e2e0143dff38575bb2ea816
alvjimen <alvjimen@student.42madrid.com>
2023-05-04 12:54:21 +0200

    Adding header file

---
+++ minirt.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvjimen <alvjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 12:49:59 by alvjimen          #+#    #+#             */
+/*   Updated: 2023/05/04 12:51:57 by alvjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef MINIRT_H
+# define MINIRT_H
+/*KEYBOARD BINDING*/
+# ifdef	__APPLE__
+#  define ESC 53
+# else
+#  define ESC 65307
+# endif
+/*WIN SIZE*/
+# define WIN_H 640
+# define WIN_W 640
+
+# include <stdio.h>
+# include <stdlib.h>
+#endif
+
