421f3993d11914c6221b7f121dca8655296a4ca5
alvjimen <alvjimen@student.42madrid.com>
2023-05-26 15:15:55 -0400

    Update things

--- sphere.c
+++ sphere.c
@@ -10,3 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "minirt.h"
+
+int	ft_hit_sphere(t_vec3d *center, double diameter, t_ray *ray)
+{
+	t_vec3d	*oc;
+	double	square_x;
+	double	square_y;
+	double	square_z;
+	double	discriminant;
+	
+
+	oc = ft_vec3d_minus_vec3d(ray->origin, center);
+	square_x = ft_vec3d_dot(ray->direction, ray->direction);
+	square_y = 2.0 * ft_vec3d_dot(oc, ray->direction);
+	square_z = ft_vec3d_dot(oc, oc) - ((diameter / 2) * (diameter / 2));
+	discriminant = square_y * square_y - 4 * square_x * square_z;
+	return (discriminant > 0);
+}
+
