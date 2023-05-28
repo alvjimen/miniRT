c4f423e7ec98d260af095c8f459104a40447b365
alvjimen <alvjimen@student.42madrid.com>
2023-05-28 10:35:04 +0100

    Simplifing the sphere hit and more readable

--- sphere.c
+++ sphere.c
@@ -6,7 +6,7 @@
 /*   By: alvjimen <alvjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:32:04 by alvjimen          #+#    #+#             */
-/*   Updated: 2023/05/28 10:16:41 by alvjimen         ###   ########.fr       */
+/*   Updated: 2023/05/28 10:34:25 by alvjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minirt.h"
@@ -20,12 +20,43 @@ double	ft_hit_sphere(t_vec3d *center, double diameter, t_ray *ray)
 	double	discriminant;
 
 	oc = ft_vec3d_minus_vec3d(ray->origin, center);
-	a = ft_vec3d_dot(ray->direction, ray->direction);
+	a = ft_vec3d_squared_len(ray->direction);
 	half_b = ft_vec3d_dot(oc, ray->unit_direction);
-	c = ft_vec3d_dot(oc, oc) - ((diameter / 2) * (diameter / 2));
+	c = ft_vec3d_squared_len(oc) - ((diameter / 2) * (diameter / 2));
 	free(oc);
 	discriminant = half_b * half_b - a * c;
 	if (discriminant < 0)
 		return (-1.0);
 	return ((-half_b - sqrt(discriminant)) / a);
 }
+/*
+int	ft_hit_sphere(t_ray *ray, t_camera *camera, t_hit_record *rec,
+		t_element *sphere)
+{
+	t_vec3d	*oc;
+	double	a;
+	double	half_b;
+	double	c;
+	double	discriminant;
+	*/
+	/*
+		double	sqrtd;
+		double	root;
+	*/
+	/*
+
+	oc = ft_vec3d_minus_vec3d(ray->origin, sphere.coords);
+	a = ft_vec3d_dot(ray->direction, ray->direction);
+	half_b = ft_vec3d_dot(oc, ray->unit_direction);
+	c = ft_vec3d_dot(oc, oc) - ((diameter / 2) * (diameter / 2));
+	free(oc);
+*/
+	/*
+	discriminant = half_b * half_b - a * c;
+	if (discriminant < 0)
+		return (0);
+	sqrtd = sqrt(discriminant);
+	
+	return ((-half_b - sqrt(discriminant)) / a);
+	*/
+//}
