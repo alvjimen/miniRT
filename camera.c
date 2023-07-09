5f4a48b83c0ac09e2d7494be6d832f223fe07f61
alvjimen <alvjimen@student.42madrid.com>
2023-07-09 11:58:14 +0200

    Norminette camera OK

--- camera.c
+++ camera.c
@@ -6,7 +6,7 @@
 /*   By: alvjimen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 18:04:11 by alvjimen          #+#    #+#             */
-/*   Updated: 2023/07/09 11:53:46 by alvjimen         ###   ########.fr       */
+/*   Updated: 2023/07/09 11:57:13 by alvjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minirt.h"
@@ -33,7 +33,7 @@ void	ft_img(t_data *img, const int image_width, const double aspect_ratio)
 	img->modifier = 0.1;
 }
 
-t_camera	ft_set_camera_vars(t_vec3d lookfrom, const double aspect_ratio, 
+t_camera	ft_set_camera_vars(t_vec3d lookfrom, const double aspect_ratio,
 		const double fov, t_vec3d lookat)
 {
 	t_camera	ptr;
@@ -61,24 +61,8 @@ t_camera	ft_init_camera(t_vec3d lookfrom, const double aspect_ratio,
 	t_vec3d		vup;
 
 	ptr = ft_set_camera_vars(lookfrom, aspect_ratio, fov, lookat);
-	/*
-	ptr.lookfrom = lookfrom;
-	ptr.lookat = lookat;
-	
-	ptr.aspect_ratio = aspect_ratio;
-	ptr.fov = fov;
-	ptr.origin = lookfrom;
-	ptr.theta = ft_degree_to_radians(fov);
-	ptr.h = tan(ptr.theta / 2);
-	ptr.viewport_height = 2.0 * ptr.h;
-	ptr.viewport_width = aspect_ratio * ptr.viewport_height;
-	ptr.focal_length = 1.0;
-	*/
-	/*Recalculate for move lookat 1*/
 	w = ft_vec3d_unit_lenght(ft_vec3d_minus_vec3d(lookfrom, lookat));
-	/*End*/
 	vup = ft_init_vec3d(0, 1, 0);
-	/*Recalculate for move camera 2*/
 	u = ft_vec3d_unit_lenght(ft_vec3d_cross(vup, w));
 	v = ft_vec3d_cross(w, u);
 	ptr.horizontal = ft_vec3d_pro_double(u, ptr.viewport_width);
@@ -87,8 +71,8 @@ t_camera	ft_init_camera(t_vec3d lookfrom, const double aspect_ratio,
 			ft_vec3d_plus_vec3d(ft_vec3d_div_double(ptr.horizontal, 2.0),
 				ft_vec3d_plus_vec3d(ft_vec3d_div_double(
 						ptr.vertical, 2.0), w)));
-	/*End*/
-	ptr.t_min = 0.0/*0001*/;//0.2
+	ptr.t_min = 0.0;
 	ptr.t_max = INFINITY;
 	return (ptr);
 }
+//	ptr.t_min = 0.00001;
